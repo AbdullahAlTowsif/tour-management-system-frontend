f3cbb16744578d8ea0566d766a532f8fba5c6243
AbdullahAlTowsif <abdullah.al.towsif2002@gmail.com>
2025-08-19 15:18:30 +0600

    tour type delete added

--- src/pages/Admin/AddTourType.tsx
+++ src/pages/Admin/AddTourType.tsx
@@ -1,4 +1,4 @@
-import { useGetTourTypesQuery } from "@/redux/features/Tour/tour.api";
+import { useGetTourTypesQuery, useRemoveTourTypeMutation } from "@/redux/features/Tour/tour.api";
 import {
   Table,
   TableBody,
@@ -10,16 +10,31 @@ import {
 import { Trash2 } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { AddTourTypeModal } from "@/components/modules/Admin/TourType/AddTourTypeModal";
+import { DeleteConfirmation } from "@/components/DeleteConfirmation";
+import { toast } from "sonner";
 
 const AddTourType = () => {
   const { data } = useGetTourTypesQuery(undefined);
+  const [removeTourType] = useRemoveTourTypeMutation();
+
+  const handleRemoveTourType = async (tourId:string) => {
+    try {
+      const res = await removeTourType(tourId).unwrap();
+      const toastId = toast.loading("Removing Tour Type");
+      if(res.success) {
+        toast.success("Removed", {id: toastId});
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
   return (
     <div className="w-full max-w-7xl mx-auto px-5">
-        <div className="flex justify-between my-8">
-            <h1 className="text-xl font-semibold">Tour Type</h1>
-            {/* <Button className="hover:cursor-pointer">Add Tour Type</Button> */}
-            <AddTourTypeModal></AddTourTypeModal>
-        </div>
+      <div className="flex justify-between my-8">
+        <h1 className="text-xl font-semibold">Tour Type</h1>
+        {/* <Button className="hover:cursor-pointer">Add Tour Type</Button> */}
+        <AddTourTypeModal></AddTourTypeModal>
+      </div>
       <div className="border border-muted rounded-md">
         <Table>
           <TableHeader>
@@ -29,10 +44,19 @@ const AddTourType = () => {
             </TableRow>
           </TableHeader>
           <TableBody>
-            {data?.data?.map((item: {name: string}) => (
+            {data?.data?.map((item: { _id: string; name: string }) => (
               <TableRow>
-                <TableCell className="font-medium w-full">{item?.name}</TableCell>
-                <TableCell className="font-medium"><Button size={"sm"}> <Trash2></Trash2> </Button></TableCell>
+                <TableCell className="font-medium w-full">
+                  {item?.name}
+                </TableCell>
+                <TableCell className="font-medium">
+                  <DeleteConfirmation onConfirm={() => handleRemoveTourType(item._id)}>
+                    <Button size={"sm"}>
+                      {" "}
+                      <Trash2></Trash2>{" "}
+                    </Button>
+                  </DeleteConfirmation>
+                </TableCell>
               </TableRow>
             ))}
           </TableBody>
